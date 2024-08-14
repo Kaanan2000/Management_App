e1d36336e31c3de46485b12a9ee4d79442e3a226
KAANAPRIYAN <kkaanapriyan@gmail.com>
2024-08-14 18:52:39 +0530

    Add initial project setup

---
+++ src/test/java/com/kaanan/management_app/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaanan.management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
