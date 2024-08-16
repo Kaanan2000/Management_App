49c3032c54c79b1e84cfd5fbaad11782944ddbd4
KAANAPRIYAN <kkaanapriyan@gmail.com>
2024-08-16 23:21:30 +0530

    Added input validation

--- src/main/java/com/kaanan/management_app/model/App_User.java
+++ src/main/java/com/kaanan/management_app/model/App_User.java
@@ -1,7 +1,9 @@
 package com.kaanan.management_app.model;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,16 +26,20 @@ public class App_User implements UserDetails {
     @NotBlank(message = "First name is required")
     private String firstName;
 
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
     @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password should be longer than 8 characters")
     private String userPassword;
 
     @NotNull(message = "Role is required")
     private Role role;
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/com/kaanan/management_app/model/Employee.java
+++ src/main/java/com/kaanan/management_app/model/Employee.java
@@ -1,5 +1,8 @@
 package com.kaanan.management_app.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +16,15 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class Employee {
     private Integer employeeId;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be less than 100 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Size(max = 50, message = "Position must be less than 50 characters")
     private String position;
 }
